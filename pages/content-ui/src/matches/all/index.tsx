d639b453428f714f47385d013171800d861ebd7d
gaspard <gaspard@gmail.com>
2025-12-15 23:24:17 +0100

    chore: remove boilerplate content-ui matches

--- pages/content-ui/src/matches/all/index.tsx
+++
@@ -1,5 +0,0 @@
-import { initAppWithShadow } from '@extension/shared'
-import App from '@src/matches/all/App'
-import inlineCss from '../../../dist/all/index.css?inline'
-
-initAppWithShadow({ id: 'CEB-extension-all', app: <App />, inlineCss })
--- pages/content-ui/src/matches/example/index.tsx
+++
@@ -1,5 +0,0 @@
-import { initAppWithShadow } from '@extension/shared'
-import App from '@src/matches/editor/App'
-import inlineCss from '../../../dist/editor/index.css?inline'
-
-initAppWithShadow({ id: 'CEB-extension-editor', app: <App />, inlineCss })
